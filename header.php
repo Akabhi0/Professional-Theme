36ae2c71af3b0be76900444a5c0fd661812618fa
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-05 00:15:59 +0530

    css check done

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+    <title><?php bloginfo('name'); ?> | <?php wp_title(); ?></title>
+	<link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet" >
+	<link href="<?php bloginfo('template_url'); ?>/bootstrap.css" rel="stylesheet">
+	<?php wp_head(); ?>
+ </head>
+ </body>
+    <div id="header">header</div>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?><!--this is the vode for adding the header section in our body or index.php-->
+	<div id="body">body</div>
+<?php get_footer(); ?><!--this is the vode for adding the footer section in our body or index.php-->
