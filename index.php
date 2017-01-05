4d2c5fdcedb131f7be19ff648816e4d5919f7242
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-05 23:48:18 +0530

    body layout

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?><!--this is the vode for adding the header section in our body or index.php-->
-	<div id="body">body</div>
+	<div id="body">
+	   <ul>
+	     <li id="javascript"></li>
+		 <li id="page"></li>
+	     <li id="widget"></li>
+	</div>
 <?php get_footer(); ?><!--this is the vode for adding the footer section in our body or index.php-->
