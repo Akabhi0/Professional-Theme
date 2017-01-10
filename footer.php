71a925de84f89f5c4ac8edbd5287c8f6c529eadb
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-10 10:32:54 +0530

    footer totly completed and body part fixed

--- footer.php
+++ footer.php
@@ -1,6 +1,37 @@
 
 
 <div id="footer">
+   <div id="footer1">
+      <ul>
+	     <li id="widget">
+		    <ul id="widget_part">
+			
+                  <li>  
+				  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('news widget') ): ?>
+	              <?php endif; ?>
+			      </li>
+				  
+				  <li>
+                  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('work widget') ): ?>
+	              <?php endif; ?>				  
+			      </li>
+				  
+				  <li>
+				  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('stay widget') ): ?>
+	              <?php endif; ?>	
+			      </li>
+				  
+				  <li id="pagess">
+	              <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('page widget') ): ?>
+	              <?php endif; ?>
+			      </li>
+				  
+			</ul>
+		 </li>
+	   </ul>	 
+   </div>
+   
+   <div id="footer2">
    <ul>
     <li>
 	 <!--this is the code for costimzable footer logo-->
@@ -17,6 +48,7 @@
 	<?php printf( __( '&copy; 2016 Ak_Abhi.  All right reserver.Designed by %s', 'Rt Camp' ),'Ak_Abhi' ); ?></a>
 	</li>
    </ul>
+   </div>
 
 <?php wp_footer(); ?>
 </div>
--- index.php
+++ index.php
@@ -1,32 +1,8 @@
 <?php get_header(); ?><!--this is the vode for adding the header section in our body or index.php-->
 	<div id="body">
-	   <ul>
-	     <li id="javascript"></li>
-		 <li id="page"></li>
-	     <li id="widget">
-		    <ul id="widget_part">
-			
-                  <li>  
-				  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('news widget') ): ?>
-	              <?php endif; ?>
-			      </li>
-				  
-				  <li>
-                  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('work widget') ): ?>
-	              <?php endif; ?>				  
-			      </li>
-				  
-				  <li>
-				  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('stay widget') ): ?>
-	              <?php endif; ?>	
-			      </li>
-				  
-				  <li>
-	              <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('page widget') ): ?>
-	              <?php endif; ?>
-			      </li>
-				  
-			</ul>
-		 </li>
+	  <ul>
+	    <li id="javascript"> javascript </li><!-- this is the part where feature image display here -->
+	    <li id="jquery"> pages </li><!-- this is the part where child page display here here -->
+	  </ul>
 	</div>
 <?php get_footer(); ?><!--this is the vode for adding the footer section in our body or index.php-->
