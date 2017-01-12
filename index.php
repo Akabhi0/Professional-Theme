061b988dca01930dc4e91c97e3690164e3ff136a
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-12 23:26:37 +0530

    carsole is added to java script

--- index.php
+++ index.php
@@ -10,19 +10,44 @@
 						$lastposts = get_posts( $args );
 						if ( $lastposts ) {
 							foreach ( $lastposts as $post ) { ?>
-							    
-								
-								<div id="write"><div id="back"><h3><?php the_title();//this is the code to show thw title of post ?></h3>
-				                <h5><?php echo substr(get_the_excerpt(),0,200); //this is the code which take the content from post?> </h5></div>
-								
-								<?php if ( has_post_thumbnail()){
-	                                 the_post_thumbnail();
-                                }?>
-								</div>
-							<?php }
-				        }
-					
-				?>
+							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+									  <!-- Indicators -->
+									  <ol class="carousel-indicators">
+										<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+										<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+										<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+									  </ol>
+
+									  <!-- Wrapper for slides -->
+									  <div class="carousel-inner" role="listbox">
+										<div class="item active">
+										  <?php  if ( has_post_thumbnail()){
+										                       the_post_thumbnail(); } ?>
+										  <div class="carousel-caption">
+										   <h3><?php the_title();//this is the code to show thw title of post ?></h3>
+				                           <?php echo substr(get_the_excerpt(),0,200); ?> 
+										  </div>
+										</div>
+										<div class="item">
+										  <img src="..." alt="...">
+										  <div class="carousel-caption">
+											...
+										  </div>
+										</div>
+										...
+									  </div>
+
+									  <!-- Controls -->
+									  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+										<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+									  </a>
+									  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+										<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+									  </a>
+									</div>    
+						<?php }  }?>
 			
                 </div>			   
 		</li><!-- this is the part where feature image display here -->
