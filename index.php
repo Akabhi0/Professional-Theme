65fea9a4d79872ca588539b19ebfa1a5a82258d6
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-13 22:24:37 +0530

    responsive done

--- index.php
+++ index.php
@@ -8,46 +8,46 @@
                    'post__in' => array(131)
                     );
 						$lastposts = get_posts( $args );
-						if ( $lastposts ) {
 							foreach ( $lastposts as $post ) { ?>
 							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-									  <!-- Indicators -->
-									  <ol class="carousel-indicators">
-										<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-										<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-										<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-									  </ol>
+								  <!-- Indicators -->
+								  <ol class="carousel-indicators">
+									<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+								  </ol>
 
-									  <!-- Wrapper for slides -->
-									  <div class="carousel-inner" role="listbox">
-										<div class="item active">
-										  <?php  if ( has_post_thumbnail()){
-										                       the_post_thumbnail(); } ?>
-										  <div class="carousel-caption">
-										   <h3><?php the_title();//this is the code to show thw title of post ?></h3>
-				                           <?php echo substr(get_the_excerpt(),0,200); ?> 
-										  </div>
-										</div>
-										<div class="item">
-										  <img src="..." alt="...">
-										  <div class="carousel-caption">
-											...
-										  </div>
-										</div>
+								  <!-- Wrapper for slides -->
+								  <div class="carousel-inner" role="listbox">
+									<div class="item active">
+									  <?php if ( has_post_thumbnail()){
+	                                 the_post_thumbnail();
+                                     }?>
+									  <div class="carousel-caption">
 										...
 									  </div>
+									</div>
+									<div class="item">
+									  <img src="..." alt="...">
+									  <div class="carousel-caption">
+										...
+									  </div>
+									</div>
+									...
+								  </div>
 
-									  <!-- Controls -->
-									  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-										<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-										<span class="sr-only">Previous</span>
-									  </a>
-									  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-										<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-										<span class="sr-only">Next</span>
-									  </a>
-									</div>    
-						<?php }  }?>
+								  <!-- Controls -->
+								  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								  </a>
+								  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								  </a>
+								</div>    
+																
+							<?php }?>
 			
                 </div>			   
 		</li><!-- this is the part where feature image display here -->
@@ -72,21 +72,21 @@
 			         echo get_the_post_thumbnail($post1); // <!--this is the code for the importing the backgorund image-->
                           $dis = get_page($post1); ?>					
 					<h4><?php echo  $dis->post_title; ?></h4>
-					<p><?php echo  substr($dis -> post_content,0,130); ?></p>
+					<?php echo  substr($dis -> post_content,0,130); ?>
 		        </li>
 				
-			    <li id="block2"><?php $post1 = $post["page2"];
+			    <li id="block2"><?php $post1 = $post["page3"];
 			         echo get_the_post_thumbnail($post1); // <!--this is the code for the importing the backgorund image-->				
 					 $dis = get_page($post1); ?>	
 					<h4><?php echo  $dis->post_title; ?></h4>
-					<p><?php echo  substr($dis -> post_content,0,130); ?></p>
+					<?php echo  substr($dis -> post_content,0,130); ?>
 				</li>
 				
-			    <li id="block3"><?php $post1 = $post["page3"];
+			    <li id="block3"><?php $post1 = $post["page2"];
 			         echo get_the_post_thumbnail($post1); // <!--this is the code for the importing the backgorund image-->				
 					 $dis = get_page($post1);?>				
 				     <h4><?php echo  $dis->post_title; ?></h4>
-					 <p><?php echo  substr($dis -> post_content,0,130);  ?></p>
+					 <?php echo  substr($dis -> post_content,0,130);  ?>
 				</li>
 				
 			 </ul>
