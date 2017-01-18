ff169cbe1b1b44a746ff153c78d1758cff974d83
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-18 15:56:31 +0530

    rework

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
 	  <ul>
 	    <li id="javascript"> 
                 <div id="pic">
-			    <?php  
-				   /* $args = array(
-                   'post__in' => array(154,127,129)
-                    );
-						$lastposts = get_posts( $args );
-							foreach ( $lastposts as $post ) { 
-							if($post == 154 || 131 || 129 ){  */?>
 							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 								  <!-- Indicators -->
 								  <ol class="carousel-indicators">
@@ -21,31 +14,17 @@
 								  <!-- Wrapper for slides -->
 								  <div class="carousel-inner" role="listbox">
 									<div class="item active">
-									    <?php /* if ( has_post_thumbnail()){
-	                                             the_post_thumbnail();
-							              } */
-									     ?>
 									  <div class="carousel-caption">
 										...
 									  </div>
 									</div>
 									<div class="item">
-									   <?php 
-									        /* if ( has_post_thumbnail()){
-	                                             the_post_thumbnail();
-						                       	 } */
-									 ?>
 									  <div class="carousel-caption">
 										...
 									  </div>
 									</div>
 								    
 									<div class="item">
-									   <?php 
-									        /* if ( has_post_thumbnail()){
-	                                             the_post_thumbnail();
-						                       	} } */
-									 ?>
 									  <div class="carousel-caption">
 										...
 									  </div>
@@ -64,7 +43,7 @@
 								  </a>
 								</div>    
 																
-							<?php /*} */ ?>
+						
 			
                 </div>			   
 		</li><!-- this is the part where feature image display here -->
