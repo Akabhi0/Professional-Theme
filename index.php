d70967e8b75734c253080a99d8b6423ecc76546b
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-01-26 09:22:37 +0530

    comminting is done in the code

--- index.php
+++ index.php
@@ -3,49 +3,8 @@
 	  <ul>
 	    <li id="javascript"> 
                 <div id="pic">
-				<?php 
-	            $htitle = get_theme_mod( 'mwsmall_home_title' );
-	            $home_text = get_theme_mod( 'mwsmall_home_text' );
-	            if ( !empty( $htitle ) || !empty( $home_text ) ) {
-                ?>
-				<section class="home_text text-center col-lg-12 col-md-12 col-sm-12">
-					<?php if ( !empty( $htitle ) ) { ?>
-					<h2><?php echo $htitle; ?></h2>
-					<?php } ?>
-					<?php if ( !empty( $home_text ) ) { ?>
-					<p><?php echo $home_text; ?></p>
-					<?php } ?>
-				</section><?php } ?>
 				
-				<section id="primary" class="container content-area col-lg-9 col-md-9 col-sm-8">
-	            <div id="content" class="site-content" role="main">
-		        <?php 
-			        $hide_slider = get_theme_mod( 'hide_slider_post' );
-			        if ( ( $hide_slider == '' && is_front_page() ) || ( $hide_slider == '' && is_home() ) ) {
-				    get_template_part( 'inc/slider-post' );
-			        }
-		        ?>
-		
-		        <?php if ( have_posts() ) : ?>
-			    <?php /* Start the Loop */ ?>
-			    <?php while ( have_posts() ) : the_post(); ?>
-			
-				<?php get_template_part( 'content', get_post_format() ); ?>
-				
-			    <?php endwhile; ?>
-			
-			    <?php mwsmall_pagination_nav(); ?>
-			
-		        <?php else : ?>
-
-			    <?php get_template_part( 'content', 'none' ); ?>
-
-		        <?php endif; ?>
-
-	            </div><!-- #content -->
-
-                </section><!-- #primary -->
-               </div>			   
+	            </div><!-- #content -->		   
 		</li><!-- this is the part where feature image display here -->
 				
 	    <li id="jquery">
@@ -57,13 +16,13 @@
 				</li>
 				
 				
-				
+				<!--this is the part of child page-->
 				<?php
                 $post = array( 38 ,52, 54) ;
 			    foreach( $post as $key ){
 				if($key == 38 || $key == 52 || $key == 54){
 			    ?>
-				
+				<!-- featuring page image title and dec -->
 			    <li id="block1">
 				    <?php
 			         echo get_the_post_thumbnail($key); // <!--this is the code for the importing the backgorund image-->
@@ -72,7 +31,6 @@
 				    <?php echo  substr($dis -> post_content,0,130); } }  ?>
 		        </li>
 				
-			    
 			 </ul>
 			 </div></center>
 		</li><!-- this is the part where child page display here here -->
--- js/js.js
+++ js/js.js
@@ -1,13 +1,15 @@
+ //------------------------------------------------------
 //add in the drop down menu in wordpress theme
+/--------------------------------------------------------
 jQuery(document).ready(function(){
   jQuery(" .menu > li").hover(function(){
-    jQuery(this).find(".sub-menu").stop().slideToggle(190);
+    jQuery(this).find(".sub-menu").stop().slideToggle(200);
 	   
 	   //this is for only drop down of 1 level drop down
-      jQuery(this).find(".sub-menu  .sub-menu > li").slideToggle(190);
+      jQuery(this).find(".sub-menu  .sub-menu > li").slideToggle(200);
 	  
 	  jQuery(" #menu-item-163 ").hover(function(){
-		  jQuery(this).find(".sub-menu li").stop().slideToggle(190,function(){
+		  jQuery(this).find(".sub-menu li").stop().slideToggle(200,function(){
 			  jQuery(".sub-menu").mouseout(function(){
 				  jQuery(this).slideDown(200);
 			  });
@@ -15,7 +17,7 @@ jQuery(document).ready(function(){
 	  });
 	  
 	  jQuery(" #menu-item-169 ").hover(function(){
-		  jQuery(this).find(".sub-menu li").stop().slideToggle(190,function(){
+		  jQuery(this).find(".sub-menu li").stop().slideToggle(200,function(){
 			  jQuery(".sub-menu").mouseout(function(){
 				  jQuery(this).slideDown(200);
 			  });
@@ -24,10 +26,12 @@ jQuery(document).ready(function(){
 	  
  });
 });
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
 
-
+//-------------------------------------------------------------------
 //this is the code for pages part 
-
+//-------------------------------------------------------------------
 jQuery(document).ready(function(){
 	jQuery("#menu-item-79").hover(function(){
 			jQuery("#block1").css({
@@ -40,4 +44,5 @@ jQuery(document).ready(function(){
 		});
 	 });
 });
-
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
